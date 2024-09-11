53156db8da8d345c2b72fd96448d460decfb683e
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-11 11:49:53 -0400

    updated

---
+++ public/src/app.jsx
@@ -0,0 +1,15 @@
+// const element = (
+//     <div title="outer div">
+//         <h1>
+//             Hello World! Bastard
+//         </h1>
+//     </div>
+// );
+
+// ReactDOM.render(element, document.getElementById("contents"));
+
+class HelloWorld extends React.Component {
+    render() {
+        const continents = ['Africa', 'Asia', 'Australia', 'Europe'];
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(3000, function () {
+    console.log('App started on port 3000');
+});
