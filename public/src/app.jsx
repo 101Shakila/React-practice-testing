07b1bed814e9679fa9207de579696bdc962b924c
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-11 12:17:35 -0400

    3 classes

--- public/src/app.jsx
+++ public/src/app.jsx
@@ -8,19 +8,45 @@
 
 // ReactDOM.render(element, document.getElementById("contents"));
 
-class HelloWorld extends React.Component {
+// class HelloWorld extends React.Component {
+//     render() {
+//         const continents = ['Africa', 'Asia', 'Australia', 'Europe'];
+//         const helloContinents = Array.from(continents, c => `Hello ${c}!`);
+//         const message = helloContinents.join(' ');
+//         return (
+//             <div title="Outer div">
+//                 <h1>{message}</h1>
+
+//             </div>
+//         );
+//     }
+
+// }
+// const element = <HelloWorld />;
+// ReactDOM.render(element, document.getElementById('contents'));
+
+class IssueRow extends React.Component {
     render() {
-        const continents = ['Africa', 'Asia', 'Australia', 'Europe'];
-        const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-        const message = helloContinents.join(' ');
         return (
-            <div title="Outer div">
-                <h1>{message}</h1>
-
-            </div>
-        );
+            <div>This is a placeholder for the issue row</div>
+        )
     }
-
 }
-const element = <HelloWorld />;
-ReactDOM.render(element, document.getElementById('contents'));
+
+
+class IssueFilter extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the issue filter</div>
+        )
+    }
+}
+
+
+class IssueTable extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the issue table</div>
+        )
+    }
+}
